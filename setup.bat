a5b508f86074bff6addd361d3fc151d879941282
SyenLxyz <syenlxyz@gmail.com>
2022-11-14 00:24:54 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 @echo off
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=1 /S /D=%UserProfile%\Miniconda3
+call conda activate
+pip install -r requirements.txt
 pause
