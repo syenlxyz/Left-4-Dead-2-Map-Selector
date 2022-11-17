fa25a4aeb844570645ba6ac3f60a19bdc29f79f6
SyenLxyz <syenlxyz@gmail.com>
2022-11-18 00:04:15 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,69 +1,61 @@
-# Import python packages
 import numpy as np
 import tkinter as tk
-from tkinter.font import Font
 
-# Draw sample from list of maps
-def get_sample():
-    # Set up list of maps
-    maps = get_sample.items
-    if not maps:
-        maps = default.copy()
-    num_map = len(maps)
+class App:
+    def __init__(self):
+        self.items = self.get_items()
     
-    # Draw sample using random weights
-    weights = np.random.poisson(size=num_map) + 1
-    probs = weights / sum(weights)
-    sample = np.random.choice(maps, p=probs)
+    @staticmethod
+    def get_items():
+        items = [
+            'Dead Center',
+            'The Passing',
+            'Dark Carnival',
+            'Swamp Fever',
+            'Hard Rain',
+            'The Parish',
+            'The Sacrifice',
+            'No Mercy',
+            'Crash Course',
+            'Death Toll',
+            'Dead Air',
+            'Blood Harvest',
+            'Cold Stream',
+            'The Last Stand'
+        ]
+        return items
     
-    # Draw sample without replacement
-    maps.remove(sample)
-    get_sample.items = maps
-    return sample
+    def select_item(self):
+        if not self.items:
+            self.items = self.get_items()
 
-# Run tk interface
-def run():
-    # Set up tkinter window 
-    window = tk.Tk()
-    window.title('L4D2 Map Selector')
-    window.geometry('300x120')
+        items = self.items
+        num_item = len(items)
 
-    # Create placeholder for selected map
-    font = Font(family='TkDefaultFont', size=15)
-    label = tk.Label(window, text='', font=font)
-    label.pack(pady=30)
+        weights = np.random.poisson(size=num_item) + 1
+        probs = weights / sum(weights)
+
+        item = np.random.choice(items, p=probs)
+        items.remove(item)
+        return item
     
-    # Create button to select map
-    def generate():
-        sample = get_sample()
-        label['text'] = sample
-    select_btn = tk.Button(window, text='Generate', command=generate)
-    select_btn.pack()
+    def run(self):
+        window = tk.Tk()
+        window.title('Left 4 Dead 2 Map Selector')
+        window.geometry('320x140')
 
-    # Launch tkinter window
-    window.mainloop()
+        label = tk.Label(window, text='Select Map', font=('TkDefaultFont', 22))
+        label.pack(pady=35)
 
-# Run the main program
-if __name__ == '__main__':
-    # Define a list of l4d2 maps
-    global default
-    default = [
-        'Dead Center',
-        'The Passing',
-        'Dark Carnival',
-        'Swamp Fever',
-        'Hard Rain',
-        'The Parish',
-        'The Sacrifice',
-        'No Mercy',
-        'Crash Course',
-        'Death Toll',
-        'Dead Air',
-        'Blood Harvest',
-        'Cold Stream',
-        'The Last Stand'
-    ]
+        def select():
+            item = self.select_item()
+            label['text'] = item
 
-    # Initialize list of maps
-    get_sample.items = default.copy()
-    run()
+        button = tk.Button(window, text='Select Map', command=select)
+        button.pack()
+
+        window.mainloop()
+
+if __name__=='__main__':
+    app = App()
+    app.run()
