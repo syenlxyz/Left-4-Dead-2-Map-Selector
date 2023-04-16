c30e3cec17056f06dd2130d6eae4ac5f551585fd
SyenLxyz <syenlxyz@gmail.com>
2023-04-16 16:45:28 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 @echo off
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=1 /S /D=%UserProfile%\Miniconda3
+set pip="python-3.9.13-embed-amd64/Scripts/pip.exe"
+%pip% install numpy
 pause
