0d8ade274b5a16a4fd2091e04d11bfb65b240b4a
SyenLxyz <syenlxyz@gmail.com>
2023-11-23 21:59:01 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-if not defined minimized set minimized=1 && start /min run.bat && exit
-set python="python-3.9.13-embed-amd64/python.exe"
-%python% run.py
-exit
+title %cd%
+set pythonw="python-3.9.13-embed-amd64/pythonw.exe"
+%pythonw% run.py
